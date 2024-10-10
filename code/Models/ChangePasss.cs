c4f6e0ae609fe1543635dd06b090ca21306a7f91
VinayRane163 <vinayrane342@gmail.com>
2024-10-10 22:46:39 +0530

    0.0.2

---
+++ code/Models/ChangePasss.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace twit.Models
+{
+    public class ChangePasss
+    {
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [StringLength(100, ErrorMessage = "Password can be a maximum of 100 characters.")]
+        public string NewPass{ get; set; }
+
+        [Compare("NewPass",ErrorMessage ="password doesnt match")]
+        public string ConfirmPass{ get; set; }
+    }
+}
